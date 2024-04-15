b6c27765582e366acb7a43a7488abbf43a4f3c94
vishalgggg <vsoftjob184@gmail.com>
2024-04-15 20:26:20 +0000

    graphql_implemented

--- src/main/java/ca/neilwhite/hrservice/graphqlcontroller/GraphQlController.java
+++ src/main/java/ca/neilwhite/hrservice/graphqlcontroller/GraphQlController.java
@@ -10,21 +10,31 @@ import reactor.core.publisher.Mono;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import ca.neilwhite.hrservice.models.Author;
+import ca.neilwhite.hrservice.models.Book;
 import ca.neilwhite.hrservice.models.requests.CreateAuthorRequest;
 import ca.neilwhite.hrservice.services.AuthorService;
+import ca.neilwhite.hrservice.services.BookService;
 
 @Slf4j
-
+@Controller
 public class GraphQlController {
     @Autowired
     private AuthorService authorService;
+    @Autowired
+    private BookService bookService;
 
     @QueryMapping("getAllAuthors")
     Flux<Author> getAllAuthors() {
-        log.info("Get all players using 'getAllAuthors' query");
+        log.info("Get all authors using 'getAllAuthors' query");
         return authorService.getAuthors();
+    }
+    @QueryMapping("getAllBooks")
+    Flux<Book> getAllBooks() {
+        log.info("Get all books using 'getAllBooks' query");
+        return bookService.getBooks();
     }
 
     @MutationMapping("addAuthor")
