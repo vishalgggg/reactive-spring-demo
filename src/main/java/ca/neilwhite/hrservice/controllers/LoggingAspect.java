8d18ad41b7dc34ac9fa431553dd8b6ff5d40a3c3
vishalggggi <vg25220@gmail.com>
2024-04-21 18:44:51 +0000

    added_vscode_profile

--- src/main/java/ca/neilwhite/hrservice/controllers/LoggingAspect.java
+++ src/main/java/ca/neilwhite/hrservice/controllers/LoggingAspect.java
@@ -16,22 +16,22 @@ public class LoggingAspect {
 
     @Around("execution(public * ca.neilwhite.hrservice.controllers.*.*(..))")
     public Object logInputAndExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
-        long t = System.currentTimeMillis();
-        log.info("=> {} :: args: {}", pjp.getSignature().toShortString(), pjp.getArgs());
+        // long t = System.currentTimeMillis();
+        // log.info("=> {} :: args: {}", pjp.getSignature().toShortString(), pjp.getArgs());
 
-        Object retVal = pjp.proceed();
+        // Object retVal = pjp.proceed();
         
-        log.info("<= {} :: Execution Time: {}ms", pjp.getSignature().toShortString(), System.currentTimeMillis() - t);
-        return retVal;
-        // ObjectMapper mapper = new ObjectMapper();
-		// String methodName = pjp.getSignature().getName();
-		// String className = pjp.getTarget().getClass().toString();
-		// Object[] array = pjp.getArgs();
-		// log.info("method invoked " + className + " : " + methodName + "()" + "arguments : "
-		// 		+ mapper.writeValueAsString(array));
-		// Object object = pjp.proceed();
-		// log.info(className + " : " + methodName + "()" + "Response : "
-		// 		+ mapper.writeValueAsString(object));
-		// return object;
+        // log.info("<= {} :: Execution Time: {}ms", pjp.getSignature().toShortString(), System.currentTimeMillis() - t);
+        // return retVal;
+        ObjectMapper mapper = new ObjectMapper();
+		String methodName = pjp.getSignature().getName();
+		String className = pjp.getTarget().getClass().toString();
+		Object[] array = pjp.getArgs();
+		log.info("method invoked " + className + " : " + methodName + "()" + "arguments : "
+				+ mapper.writeValueAsString(array));
+		Object object = pjp.proceed();
+		log.info(className + " : " + methodName + "()" + "Response : "
+				+ mapper.writeValueAsString(object));
+		return object;
     }
 }
--- src/main/java/ca/neilwhite/hrservice/models/requests/CreateAuthorRequest.java
+++ src/main/java/ca/neilwhite/hrservice/models/requests/CreateAuthorRequest.java
@@ -2,9 +2,19 @@ package ca.neilwhite.hrservice.models.requests;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    
-public record CreateAuthorRequest(@NotNull @NotEmpty String firstName, @NotNull @NotEmpty String lastName) {
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAuthorRequest  {
+    @NotNull @NotEmpty String firstName;
+    @NotNull @NotEmpty String lastName;
 }
 
 
